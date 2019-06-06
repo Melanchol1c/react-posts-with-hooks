60d553f7f8208bb7c4618ba746aad1235de38145
Melanchol1c <ricksanford498@gmail.com>
2019-06-07 04:30:07 +0700

    Test pre commit

---
+++ src/Router.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import { Switch, Route } from 'react-router-dom';
+
+
+const Router = () => (
+  <Switch>
+    <Route expact path="/" component={Player} />
+    <Route path="/roster/:number" component={Player} />
+  </Switch>
+);
